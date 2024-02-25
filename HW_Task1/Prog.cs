1233ed06fa6fe563078a46ccf5738cb76b2f4837
vad1m24 <vadim.rutkevich@gmail.com>
2024-02-25 22:25:23 +0300

    first task

---
+++ HW_Task1/Prog.cs
@@ -0,0 +1,18 @@
+Console.Clear();
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int number = Prompt ("Пользователь, введите число N = ");
+
+void NaturalNumbers(int num)
+{
+    if (num == 0) return;
+    Console.Write($"{num} ");
+    NaturalNumbers(num-1);
+}
+
+NaturalNumbers(number);
